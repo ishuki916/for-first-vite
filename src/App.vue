2942fc52983949f01d09f92650ffb084771dd9dd
Maki <makiliu@gateweb.com.tw>
2024-08-01 11:35:27 +0800

    week 1 level 2

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,123 @@
 <script setup>
 import { ref } from 'vue'
-
-const searching = ref('')
-const checkSpace = () => {
-  if (searching.value.trim() === '') {
-    alert('不可輸入空白')
+const drinkArr = ref([
+  {
+    id: 1,
+    name: '珍珠奶茶',
+    description: '香濃奶茶搭配QQ珍珠',
+    price: 50,
+    amount: 20
+  },
+  {
+    id: 2,
+    name: '冬瓜檸檬',
+    description: '清新冬瓜配上新鮮檸檬',
+    price: 45,
+    amount: 18
+  },
+  {
+    id: 3,
+    name: '翡翠檸檬',
+    description: '綠茶與檸檬的完美結合',
+    price: 55,
+    amount: 34
+  },
+  {
+    id: 4,
+    name: '四季春茶',
+    description: '香醇四季春茶，回甘無比',
+    price: 45,
+    amount: 10
+  },
+  {
+    id: 5,
+    name: '阿薩姆奶茶',
+    description: '阿薩姆紅茶搭配香醇鮮奶',
+    price: 50,
+    amount: 25
+  },
+  {
+    id: 6,
+    name: '檸檬冰茶',
+    description: '檸檬與冰茶的清新組合',
+    price: 45,
+    amount: 20
+  },
+  {
+    id: 7,
+    name: '芒果綠茶',
+    description: '芒果與綠茶的獨特風味',
+    price: 55,
+    amount: 18
+  },
+  {
+    id: 8,
+    name: '抹茶拿鐵',
+    description: '抹茶與鮮奶的絕配',
+    price: 60,
+    amount: 20
   }
+])
+const tempAmount = ref()
+
+const addAmount = (id) => {
+  const index = drinkArr.value.findIndex((obj) => obj.id === id)
+  drinkArr.value[index].amount++
+}
+
+const minusAmount = (id) => {
+  const index = drinkArr.value.findIndex((obj) => obj.id === id)
+  drinkArr.value[index].amount--
 }
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#">Week 1</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Week 2</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Week 3</a>
           </li>
         </ul>
-        <form class="d-flex">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-            @keydown.enter="checkSpace"
-            v-model="searching"
-          />
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
+
   <div class="container mt-4">
-    <p>Your are searching...{{ searching }}</p>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">品項</th>
+          <th scope="col">描述</th>
+          <th scope="col">價格</th>
+          <th scope="col">庫存</th>
+        </tr>
+      </thead>
+      <tbody v-for="(item, key) in drinkArr" :key="item.id">
+        <tr>
+          <td>{{ item.name }}</td>
+          <td>
+            <small>{{ item.description }}</small>
+          </td>
+          <td>{{ item.price }}</td>
+          <td>
+            <button type="button" class="btn btn-outline-info" @click="minusAmount(item.id)">
+              -
+            </button>
+            {{ item.amount }}
+            <button type="button" class="btn btn-outline-info" @click="addAmount(item.id)">
+              +
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
-
-<script></script>
