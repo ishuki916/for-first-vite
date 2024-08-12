af723268226dea5a6cc43c776ae72b7df57b7219
Maki <makiliu@gateweb.com.tw>
2024-08-12 18:14:42 +0800

    complete authorization and logout

--- src/views/SecondWeek.vue
+++ src/views/SecondWeek.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 import axios from 'axios'
 
 const user = ref({
-  email: '',
-  password: ''
+  email: '123@gmail.com',
+  password: '12345678'
 })
 const newUser = ref({
   email: '',
@@ -14,11 +14,19 @@ const newUser = ref({
 const loginErrMsg = ref('')
 const signUpErrMsg = ref('')
 const showModal = ref(false)
+const showTodo = ref(false)
+const showLogin = ref(true)
+const nickName = ref('')
 
 const login = async () => {
   loginErrMsg.value = ''
   try {
     const res = await axios.post('https://todolist-api.hexschool.io/users/sign_in', user.value)
+    showLogin.value = false
+    showTodo.value = true
+    console.log(res)
+    document.cookie = `tkn=${res.data.token}; expires=${res.data.exp};path=/; `
+    nickName.value = res.data.nickname
   } catch (err) {
     console.log(err.response.data.message)
     loginErrMsg.value = err.response.data.message
@@ -36,10 +44,35 @@ const signUp = async () => {
     signUpErrMsg.value = err.response.data.message
   }
 }
+
+const signOut = async () => {
+  try {
+    const myCookie = document.cookie.replace(/(?:(?:^|.*;\s*)tkn\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+    const res = await axios.post(
+      'https://todolist-api.hexschool.io/users/sign_out',
+      {},
+      {
+        headers: {
+          authorization: myCookie
+        }
+      }
+    )
+
+    if (res) {
+      showLogin.value = true
+      showTodo.value = false
+      document.cookie = `tkn=; max-age=0;path=/; `
+    } else {
+      alert('有錯誤')
+    }
+  } catch (err) {
+    console.log(err.response.data.message)
+  }
+}
 </script>
 
 <template>
-  <section class="vh-100">
+  <section class="vh-100" v-if="showLogin">
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col col-xl-10">
@@ -154,6 +187,89 @@ const signUp = async () => {
           <button type="button" class="btn btn-secondary" @click="showModal = false">關閉</button>
           <button type="button" class="btn btn-primary" @click="signUp">確定</button>
           <p>{{ signUpErrMsg }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div v-if="showTodo">
+    <div class="dropdown text-end mb-3">
+      <a
+        href="#"
+        class="d-block link-dark text-decoration-none dropdown-toggle"
+        id="dropdownUser1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <img
+          src="https://github.com/mdo.png"
+          alt="mdo"
+          width="32"
+          height="32"
+          class="rounded-circle"
+        />
+        {{ nickName }}
+      </a>
+      <ul class="dropdown-menu dropdown-menu-end text-small" aria-labelledby="dropdownUser1">
+        <li><a class="dropdown-item" href="#" @click="signOut">Sign out</a></li>
+      </ul>
+    </div>
+
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+          <div class="card" style="border-radius: 15px">
+            <div class="card-body p-5">
+              <h4 class="mb-3">To-do List</h4>
+
+              <form class="d-flex justify-content-center mb-4">
+                <div class="flex-fill">
+                  <input
+                    type="text"
+                    id="form3"
+                    class="form-control form-control-lg"
+                    placeholder="What do you need to do today?"
+                  />
+                </div>
+                <button type="submit" class="btn btn-warning btn-lg ms-2">Add</button>
+              </form>
+
+              <ul class="list-group mb-0">
+                <li
+                  class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2"
+                >
+                  <div class="d-flex align-items-center">
+                    <input
+                      class="form-check-input me-2"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                    />
+                    Cras justo odio
+                  </div>
+                  <div>
+                    <button type="button" class="btn btn-warning">edit</button>
+                    <button type="button" class="btn btn-danger">remove</button>
+                  </div>
+                </li>
+
+                <li
+                  class="list-group-item d-flex d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2"
+                >
+                  <div class="d-flex align-items-center">
+                    <input
+                      class="form-check-input me-2"
+                      type="checkbox"
+                      value=""
+                      aria-label="..."
+                      checked
+                    />
+                    <s>Dapibus ac facilisis in</s>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
